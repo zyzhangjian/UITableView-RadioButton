34b511d8851ae5be3c8584f7403cc77a129fe791
+ <+>
2016-03-22 11:09:27 +0800

    first commit

---
+++ Test/myTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  myTableViewCell.h
+//  Test
+//
+//  Created by 张健 on 16/3/21.
+//  Copyright © 2016年 张健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myTableViewCell : UITableViewCell
+
+@end
---
+++ Test/myTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  myTableViewCell.m
+//  Test
+//
+//  Created by 张健 on 16/3/21.
+//  Copyright © 2016年 张健. All rights reserved.
+//
+
+#import "myTableViewCell.h"
+
+@implementation myTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
